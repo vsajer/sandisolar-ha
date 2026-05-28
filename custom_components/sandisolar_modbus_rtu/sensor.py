f5ccd58e8a135eb4dc0a22c7bea5ccda79c11217
vsajer <67804653+vsajer@users.noreply.github.com>
2026-05-28 11:28:47 +0200

    Update sensor.py

--- custom_components/sandisolar_modbus_rtu/sensor.py
+++ custom_components/sandisolar_modbus_rtu/sensor.py
@@ -326,19 +326,19 @@ async def async_setup_entry(hass, entry, async_add_entities):
                      UnitOfEnergy.KILO_WATT_HOUR, "mdi:home-lightning-bolt"),
     ]
 
-    # Faults & warnings + TEXTOVÉ SENZORY
-    entities += [
-        FaultSensor(hub, 24, "Fault_word0"),
-        FaultWord0TextSensor(hub, 24, "Fault_word0_text"),
+# Faults & warnings + TEXTOVÉ SENZORY
+   entities += [
+        FaultSensor(hub, "fault_word0", "Fault_word0"),
+        FaultWord0TextSensor(hub, "fault_word0", "Fault_word0_text"),
 
-        WarningMainSensor(hub, 33, "Warning_main"),
-        WarningMainTextSensor(hub, 33, "Warning_main_text"),
+        WarningMainSensor(hub, "warning_main", "Warning_main"),
+        WarningMainTextSensor(hub, "warning_main", "Warning_main_text"),
 
-        WarningSubSensor(hub, 35, "Warning_sub"),
-        WarningSubTextSensor(hub, 35, "Warning_sub_text"),
+        WarningSubSensor(hub, "warning_sub", "Warning_sub"),
+        WarningSubTextSensor(hub, "warning_sub", "Warning_sub_text"),
 
-        BatteryStatusTextSensor(hub, 130, "Battery_status_text"),
-        GridStatusTextSensor(hub, 0, "Grid_status_text"),
-    ]
+        BatteryStatusTextSensor(hub, "battery_status", "Battery_status_text"),
+        GridStatusTextSensor(hub, "inverter_status", "Grid_status_text"),
+]
 
     async_add_entities(entities)

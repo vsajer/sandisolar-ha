967093e0b8a233436a6324484902e32c76534a22
vsajer <67804653+vsajer@users.noreply.github.com>
2026-05-28 09:13:57 +0200

    Update number.py

--- custom_components/sandisolar_modbus_rtu/number.py
+++ custom_components/sandisolar_modbus_rtu/number.py
@@ -13,20 +13,20 @@ async def async_setup_entry(hass, entry, async_add_entities):
     entities = [
         SandiSolarNumber(
             hub,
-            300,   # Modbus register
+            "charge_limit",
             "Charge Current Limit",
             UnitOfElectricCurrent.AMPERE,
             0,
-            200,
+            100,
             "mdi:battery-charging",
         ),
         SandiSolarNumber(
             hub,
-            301,   # Modbus register
+            "discharge_limit",
             "Discharge Current Limit",
             UnitOfElectricCurrent.AMPERE,
             0,
-            200,
+            100,
             "mdi:battery-minus",
         ),
     ]
@@ -38,12 +38,12 @@ class SandiSolarNumber(NumberEntity):
     _attr_has_entity_name = True
     _attr_mode = "slider"
 
-    def __init__(self, hub, register, name, unit, min_value, max_value, icon):
+    def __init__(self, hub, key, name, unit, min_value, max_value, icon):
         self._hub = hub
-        self._register = register
+        self._key = key
 
         self._attr_name = name
-        self._attr_unique_id = f"sandisolar_number_{register}"
+        self._attr_unique_id = f"sandisolar_number_{key}"
         self._attr_native_unit_of_measurement = unit
         self._attr_native_min_value = min_value
         self._attr_native_max_value = max_value
@@ -60,10 +60,10 @@ class SandiSolarNumber(NumberEntity):
 
     @property
     def native_value(self):
-        return self._hub._cache.get(self._register)
+        return self._hub._cache.get(self._key)
 
     async def async_update(self):
-        await self._hub.read_holding_register(self._register)
+        await self._hub.read_holding_register(self._key)
 
     async def async_set_native_value(self, value: float):
-        await self._hub.write_holding_register(self._register, int(value))
+        await self._hub.write_holding_register(self._key, value)

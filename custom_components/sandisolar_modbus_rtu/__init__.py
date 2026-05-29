e105cfdb366a3f7e61f45d8c7fd7deb4f1d3b230
vsajer <67804653+vsajer@users.noreply.github.com>
2026-05-29 23:23:19 +0200

    Update __init__.py

--- custom_components/sandisolar_modbus_rtu/__init__.py
+++ custom_components/sandisolar_modbus_rtu/__init__.py
@@ -3,7 +3,6 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, PLATFORMS
 from .hub import SandiSolarModbusHub
-from .coordinator import SandiSolarCoordinator
 
 
 async def async_setup_entry(
@@ -15,12 +14,8 @@ async def async_setup_entry(
     hub = SandiSolarModbusHub(hass, entry)
     await hub.async_init()
 
-    coordinator = SandiSolarCoordinator(hass, hub)
-    await coordinator.async_config_entry_first_refresh()
-
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
         "hub": hub,
-        "coordinator": coordinator,
     }
 
     await hass.config_entries.async_forward_entry_setups(
@@ -44,7 +39,6 @@ async def async_unload_entry(
 
     if unload_ok:
         data = hass.data[DOMAIN].pop(entry.entry_id)
-
         hub = data["hub"]
         await hub.close()
 
